1e8ac3480cf24003a8101392e7eebf9582cc09aa
=david KENEY <davidmyken@gmail.com>
2018-01-09 06:37:27 +0100

    Refactoring

--- Order.js
+++ Order.js
@@ -10,38 +10,10 @@ your_order("is2 Thi1s T4est 3a") return  "Thi1s is2 3a T4est"
 */
 
 
+
+
 function order(words) {
-    if (words) {
-        var output = "";
-        var tab = [];
-        var regex = new RegExp(/\d/);
-        words = words.split(" ");
-
-        for (var i = 0; i < words.length; i++)
-            tab.push((words[i].match(regex))[0]);
-
-        tab = tab.sort();
-
-        for (var i = 0; i < tab.length; i++) {
-            for (var j = 0; j < tab.length; j++) {
-                if (words[j].includes(tab[i])) {
-                    output += words[j] + " ";
-                }
-            }
-        }
-
-        return output.trim();
-    }
-    return words;
-}
-
-/* refactoring 
-function order(words){
-  
-    return words.split(' ').sort(function(a, b){
+    return words.split(' ').sort(function (a, b) {
         return a.match(/\d/) - b.match(/\d/);
-     }).join(' ');
-  }    
-
-
-  */
+    }).join(' ');
+}
